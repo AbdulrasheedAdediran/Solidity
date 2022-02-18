e0917415a7713aa649dfbff8c91bb347febd99b1
Abdulrasheed <abdrasheed1103@gmail.com>
2022-02-18 20:28:35 +0100

    proof of exixtence

--- EPL/EPL.sol
+++ EPL/EPL.sol
@@ -12,11 +12,8 @@ import "./quickSortAlgorithm.sol";
 // 6. promote function that replaces relegated clubs with promoted clubs
 // 6. viewTable function should display qualified clubs with promoted clubs/without relegated clubs
 // 7. viewClub function should search table by ticker and display queried club with corresponding data and error message if club is not registered
-
-
 contract EPL {
-
-
+    
     struct Club {
         uint id;
         string name;
@@ -32,10 +29,29 @@ contract EPL {
     uint[] pointArr;
     Club[5] sortedClubArr;
     mapping (uint => Club) public clubID;
-    mapping(string => bool) checkClubTicker;
-    
+    mapping(string => Club) private getSingleClub;
 
+   // 1. addClub function that takes in all club names and club tickers(for confirming proof of existence)
+    function addClub (string memory _clubName,
+                    string memory _clubTicker) public {
+        // Requires that club has not been registered 
+        require(getSingleClub[_clubTicker].id == 0, "This Football Club has already been registered");
+        Club memory newClub = Club(currentID, _clubName, _clubTicker, 0);
+        getSingleClub[_clubTicker] = newClub;
+        clubs.push(newClub);
+        clubID[currentID] = newClub;
+        currentID++;
+    }
 
+    function checkClub(string memory _clubTicker) public view returns(Club memory) {
+        require(getSingleClub[_clubTicker].id != 0, "This club has not been registered");
+        return  getSingleClub[_clubTicker];
+    }
+
+    function viewTable() public view returns(Club[] memory) {
+        return clubs;
+    }
+  
     // returns random number that ranges between 60 and 100  
     function randomPoints () public returns(uint) {
         uint mod = 40;
@@ -44,22 +60,6 @@ contract EPL {
         return result + 60;
     }
 
-   // 1. addClub function that takes in all club names and club tickers(for confirming proof of existence)
-    function addClub (string memory _clubName,
-                    string memory _clubTicker) public {
-        // Requires that club has not been registered 
-        require(checkClubTicker[_clubTicker] == false, "This Football Club has already been registered");
-        Club memory newClub = Club(currentID, _clubName, _clubTicker, 0);
-        clubs.push(newClub);
-        clubID[currentID] = newClub;
-        checkClubTicker[_clubTicker] = true;
-        currentID++;
-    }
-
-    function viewTable() public view returns(Club[] memory) {
-        return clubs;
-    }
-  
     // Show table at the end of season
     function simulateSeason() public returns(Club[5] memory) {
         
